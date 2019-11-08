a41b96629adfafd04e71f33caa9659802e6cfa9d
rafikapebe <54924523+rafikapebe@users.noreply.github.com>
2019-11-08 17:10:57 +0700

    5/rafika (#8)
    * feat: ruanganController for view ruangan details
    
    * change JPA parameter
    
    * fixing fitur for view ruangan detail

---
+++ siruangan/src/main/java/com/apap/tugasakhir/siruangan/controller/RuanganController.java
@@ -0,0 +1,36 @@
+package com.apap.tugasakhir.siruangan.controller;
+
+import com.apap.tugasakhir.siruangan.model.FasilitasModel;
+import com.apap.tugasakhir.siruangan.model.RuanganModel;
+import com.apap.tugasakhir.siruangan.service.FasilitasService;
+import com.apap.tugasakhir.siruangan.service.RuanganService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class RuanganController {
+    @Qualifier("ruanganServiceImpl")
+
+    @Autowired
+    private RuanganService ruanganService;
+
+    @Autowired
+    private FasilitasService fasilitasService;
+
+    @RequestMapping(value = "/ruangan/{id}", method = RequestMethod.GET)
+    public String viewRuangan(@PathVariable int id, Model model){
+        RuanganModel ruangan = ruanganService.getRuanganById(id);
+        List<FasilitasModel> listFasilitas = ruangan.getFasilitasList();
+        model.addAttribute("ruangan", ruangan);
+        model.addAttribute("listFasilitas", listFasilitas);
+        return "view-ruangan";
+    }
+}
