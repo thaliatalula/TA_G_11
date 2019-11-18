964c91f07564c22d9ca455426860267ee978c45e
ali <ali.yusuf@ui.ac.id>
2019-11-18 16:19:37 +0700

    tes

--- src/main/java/com/apap/tugasakhir/siruangan/service/PengadaanFasilitasServiceImpl.java
+++ src/main/java/com/apap/tugasakhir/siruangan/service/PengadaanFasilitasServiceImpl.java
@@ -4,8 +4,8 @@ import com.apap.tugasakhir.siruangan.model.PengadaanFasilitasModel;
 import com.apap.tugasakhir.siruangan.repository.PengadaanFasilitasDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
