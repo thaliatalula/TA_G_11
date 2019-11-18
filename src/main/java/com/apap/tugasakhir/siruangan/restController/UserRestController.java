1a57a44699a454dbf1ed0128891d4faf57779fa9
aliysf <47196238+aliysf@users.noreply.github.com>
2019-11-18 16:02:35 +0700

    bantu yudha (#21)

--- src/main/java/com/apap/tugasakhir/siruangan/restController/UserRestController.java
+++ src/main/java/com/apap/tugasakhir/siruangan/restController/UserRestController.java
@@ -1,20 +1,24 @@
 package com.apap.tugasakhir.siruangan.restController;
 
+import com.apap.tugasakhir.siruangan.rest.StatusDetail;
+import com.apap.tugasakhir.siruangan.restService.RuanganRestService;
 import com.apap.tugasakhir.siruangan.restService.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserRestController {
     @Autowired
     private UserRestService userRestService;
+    @Autowired
+    private RuanganRestService ruanganRestService;
 
-
-
-//    @GetMapping("/")
-//    public Mono<SiswaDetailResp> x(){
-//        return userRestService.addSiswa();
-//    }
+    @GetMapping("/")
+    public Mono<StatusDetail> x(){
+        return ruanganRestService.getStatus("2");
+    }
 }
