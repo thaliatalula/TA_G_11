f80c643ec5815b49ec8fcdaa2846be7e7ff22e9b
aliysf <47196238+aliysf@users.noreply.github.com>
2019-11-19 21:08:32 +0700

    8 web service si tu/ali (#24)
    * bantu yudha
    
    * bantu yudha
    
    * bantu yudha
    
    * bantu yudha
    
    * tes
    
    * tes
    
    * tes

--- src/main/java/com/apap/tugasakhir/siruangan/service/FasilitasServiceImpl.java
+++ src/main/java/com/apap/tugasakhir/siruangan/service/FasilitasServiceImpl.java
@@ -4,10 +4,12 @@ import com.apap.tugasakhir.siruangan.model.FasilitasModel;
 import com.apap.tugasakhir.siruangan.repository.FasilitasDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class FasilitasServiceImpl implements FasilitasService{
 
     @Autowired
--- src/main/java/com/apap/tugasakhir/siruangan/service/RuanganServiceImpl.java
+++ src/main/java/com/apap/tugasakhir/siruangan/service/RuanganServiceImpl.java
@@ -4,10 +4,12 @@ import com.apap.tugasakhir.siruangan.model.RuanganModel;
 import com.apap.tugasakhir.siruangan.repository.RuanganDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class RuanganServiceImpl implements RuanganService {
 
     @Autowired
