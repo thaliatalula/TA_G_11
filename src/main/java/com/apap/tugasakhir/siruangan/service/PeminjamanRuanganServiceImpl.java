5cc7bfb2f3b27eefc46dbb3585a31786d334d39a
Yudha Ananda Rahayu Putra <yudhaananda.rp@gmail.com>
2019-12-06 08:04:41 +0700

    update fitur 6, 8, 10

--- src/main/java/com/apap/tugasakhir/siruangan/service/PeminjamanRuanganServiceImpl.java
+++ src/main/java/com/apap/tugasakhir/siruangan/service/PeminjamanRuanganServiceImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,7 +22,33 @@ public class PeminjamanRuanganServiceImpl implements PeminjamanRuanganService {
     }
 
     @Override
-    public PeminjamanRuanganModel addPeminjamanRUangan(PeminjamanRuanganModel peminjamanRuanganModel) {
+    public PeminjamanRuanganModel addPeminjamanRuangan(PeminjamanRuanganModel peminjamanRuanganModel) {
         return peminjamanRuanganDB.save(peminjamanRuanganModel);
+    }
+
+    @Override
+    public boolean canPinjamWaktu(PeminjamanRuanganModel peminjamanRuanganModel) throws ParseException {
+        SimpleDateFormat formatWaktu= new SimpleDateFormat("HH:mm");
+        RuanganModel ruangan= peminjamanRuanganModel.getRuangan();
+        for (PeminjamanRuanganModel peminjaman: ruangan.getPeminjamanRuanganList()
+             ) {
+            if(peminjamanRuanganModel.getTanggalMulai().getTime()>peminjaman.getTanggalSelesai().getTime() || peminjamanRuanganModel.getTanggalSelesai().getTime()<peminjaman.getTanggalMulai().getTime() ){
+                if(formatWaktu.parse(peminjamanRuanganModel.getWaktuMulai()).getTime()>formatWaktu.parse(peminjaman.getWaktuSelesai()).getTime() || formatWaktu.parse(peminjamanRuanganModel.getWaktuSelesai()).getTime()<formatWaktu.parse(peminjaman.getWaktuMulai()).getTime()){
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean canPinjamKapasitas(PeminjamanRuanganModel peminjamanRuanganModel) {
+        RuanganModel ruangan= peminjamanRuanganModel.getRuangan();
+        if(ruangan.getKapasitas()< peminjamanRuanganModel.getJumlahPeserta()){
+            return false;
+        }
+        return true;
     }
 }
