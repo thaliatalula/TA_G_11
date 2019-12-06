8d65b2b1a923ae08d5dff974a21df9fe11a7fb25
aliysf <47196238+aliysf@users.noreply.github.com>
2019-12-06 21:06:39 +0700

    8 web service si tu/ali (#62)
    * bantu yudha
    
    * bantu yudha
    
    * bantu yudha
    
    * bantu yudha
    
    * tes
    
    * tes
    
    * tes
    
    * bantu webservice pinjam ruangan
    
    * bantu webservice pinjam ruangan
    
    * refactor model
    
    * fix: webservice si koperasi
    
    * fix:add user for sikoperasi
    
    * fix: admintu
    
    * min
    
    * v1, tinggal testing api SITU
    
    * fix
    
    * fix
    
    * fix la kelar fitur 8
    
    * styling nya yudha
    
    * cool html css

--- src/main/java/com/apap/tugasakhir/siruangan/service/PeminjamanRuanganServiceImpl.java
+++ src/main/java/com/apap/tugasakhir/siruangan/service/PeminjamanRuanganServiceImpl.java
@@ -8,7 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @Service
@@ -17,6 +21,13 @@ public class PeminjamanRuanganServiceImpl implements PeminjamanRuanganService {
     @Autowired
     private PeminjamanRuanganDB peminjamanRuanganDB;
 
+    public long getTimeFromFormat(Date date, String time) throws ParseException {
+        SimpleDateFormat formatDate= new SimpleDateFormat("yyyy-MM-dd");
+        String dateTime= formatDate.format(date);
+        dateTime=dateTime.concat(" "+time);
+        SimpleDateFormat formatAll=  new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return formatAll.parse(dateTime).getTime();
+    }
     @Override
     public void updateStatus(PeminjamanRuanganModel peminjamanRuanganModel) {
         PeminjamanRuanganModel peminjaman=peminjamanRuanganDB.findById(peminjamanRuanganModel.getId()).get();
@@ -42,12 +53,12 @@ public class PeminjamanRuanganServiceImpl implements PeminjamanRuanganService {
 
     @Override
     public boolean canPinjamWaktu(PeminjamanRuanganModel peminjamanRuanganModel) throws ParseException {
-        SimpleDateFormat formatWaktu= new SimpleDateFormat("HH:mm");
         RuanganModel ruangan= peminjamanRuanganModel.getRuangan();
-
         for (PeminjamanRuanganModel peminjaman: ruangan.getPeminjamanRuanganList()
              ) {
-            if(peminjamanRuanganModel.getTanggalMulai().getTime()+formatWaktu.parse(peminjamanRuanganModel.getWaktuMulai()).getTime()<=peminjaman.getTanggalSelesai().getTime()+formatWaktu.parse(peminjaman.getWaktuSelesai()).getTime() && peminjamanRuanganModel.getTanggalSelesai().getTime()+formatWaktu.parse(peminjamanRuanganModel.getWaktuSelesai()).getTime()>=peminjaman.getTanggalMulai().getTime()+formatWaktu.parse(peminjaman.getWaktuMulai()).getTime() ){
+
+            if( getTimeFromFormat(peminjamanRuanganModel.getTanggalMulai(),peminjamanRuanganModel.getWaktuMulai())<=getTimeFromFormat(peminjaman.getTanggalSelesai(),peminjaman.getWaktuSelesai())
+                    && getTimeFromFormat(peminjamanRuanganModel.getTanggalSelesai(),peminjamanRuanganModel.getWaktuSelesai())>=getTimeFromFormat(peminjaman.getTanggalMulai(),peminjaman.getWaktuMulai()) ){
                    return false;
             }
         }
